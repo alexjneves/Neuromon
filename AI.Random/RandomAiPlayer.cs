3ffa9e8eb365acd3a25cf48ad00ac961cd919f7a
Alex Neves <alexneves08@gmail.com>
2015-12-01 11:06:01 +0000

    Random AI
    Added an assembly for the random AI.
    This AI has completely random behaviour and exhibits no intelligence.

---
+++ AI.Random/RandomAiPlayer.cs
@@ -0,0 +1,28 @@
+﻿using Common;
+
+namespace AI.Random
+{
+    internal sealed class RandomAiPlayer : IPlayer
+    {
+        public string Name { get; }
+        public Neuromon Neuromon { get; }
+
+        private readonly System.Random _rand;
+
+        public RandomAiPlayer(string name, Neuromon neuromon)
+        {
+            Name = name;
+            Neuromon = neuromon;
+
+            _rand = new System.Random();
+        }
+
+        public Turn ChooseTurn()
+        {
+            var move = _rand.Next(1, 5);
+            var selectedMove = Neuromon.MoveSet.GetMove(move);
+
+            return new Turn(selectedMove);
+        }
+    }
+}
---
+++ AI.Random/RandomAiPlayerFactory.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace AI.Random
+{
+    public sealed class RandomAiPlayerFactory : IAiPlayerFactory
+    {
+        public IPlayer CreatePlayer(string name, Neuromon neuromon)
+        {
+            return new RandomAiPlayer(name, neuromon);
+        }
+    }
+}
