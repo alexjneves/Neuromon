f663cbe1726f6dfce85c03fce1de8116c1904785
Alex Neves <alexneves08@gmail.com>
2016-02-11 16:51:46 +0000

    Solution Fix, AI.Neat Project, SharpNEAT
    Fixed the solution file which was broken due to previous renaming of AI
    projects.
    Added the AI.Neat project.
    Added the SharpNEAT library.

---
+++ Player.AI.Neat/NeatAiPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Common;
+using Common.Turn;
+using SharpNeat.Phenomes;
+
+namespace Player.AI.Neat
+{
+    internal sealed class NeatAiPlayer : IPlayer
+    {
+        public string Name { get; }
+        public NeuromonCollection Neuromon { get; }
+        public Neuromon ActiveNeuromon { get; set; }
+
+        public NeatAiPlayer(string name, NeuromonCollection neuromon)
+        {
+            Name = name;
+            Neuromon = neuromon;
+        }
+
+        public ITurn ChooseTurn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Neuromon SelectActiveNeuromon()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
