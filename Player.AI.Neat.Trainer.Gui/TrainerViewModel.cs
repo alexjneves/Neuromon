32a97868d9fcfad18bcbd949c89fa1787b4ba04f
Alex Neves <alexneves08@gmail.com>
2016-02-26 17:54:40 +0000

    Evolution Algorithm Settings Grid
    Added a grid for entering evolution algorithm settings. Fields are bound
    to an
    NeatEvolutionAlgorithmParameters instance.

--- Player.AI.Neat.Trainer.Gui/TrainerViewModel.cs
+++ Player.AI.Neat.Trainer.Gui/TrainerViewModel.cs
@@ -1,8 +1,11 @@
+using SharpNeat.EvolutionAlgorithms;
+
 namespace Player.AI.Neat.Trainer.Gui
 {
     public class TrainerViewModel
     {
         public TrainingGameSettings TrainingGameSettings { get; set; }
         public ExperimentSettings ExperimentSettings { get; set; }
+        public NeatEvolutionAlgorithmParameters NeatEvolutionAlgorithmParameters { get; set; }
     }
 }
