55f583236d78f363a3f725185c582f6cb197cdac
Alex Neves <alexneves08@gmail.com>
2016-02-29 13:29:50 +0000

    Save settings, restore defaults
    Added the option of saving configuration settings.
    Default settings can also be restored through a UI button

---
+++ Player.AI.Neat.Trainer.Gui/JsonSettingsIO.cs
@@ -0,0 +1,77 @@
+using System.IO;
+using Newtonsoft.Json;
+using SharpNeat.EvolutionAlgorithms;
+
+namespace Player.AI.Neat.Trainer.Gui
+{
+    internal sealed class JsonSettingsIO
+    {
+        private const string FolderPrefix = "Config/";
+        private const string DefaultPrefix = "Default/Default";
+
+        private const string ExperimentSettingsFileName = "ExperimentSettings.json";
+        private const string EvolutionAlgorithmParametersFileName = "EvolutionAlgorithmParameters.json";
+        private const string TrainingGameSettingsFileName = "TrainingGameSettings.json";
+
+        public ExperimentSettings ReadExperimentSettings()
+        {
+            var experimentSettingsFilePath = $"{FolderPrefix}{ExperimentSettingsFileName}";
+
+            return ReadSettings<ExperimentSettings>(experimentSettingsFilePath);
+        }
+
+        public ExperimentSettings ReadDefaultExperimentSettings()
+        {
+            var defaultExperimentSettingsFilePath = $"{FolderPrefix}{DefaultPrefix}{ExperimentSettingsFileName}";
+
+            return ReadSettings<ExperimentSettings>(defaultExperimentSettingsFilePath);
+        }
+
+        public NeatEvolutionAlgorithmParameters ReadEvolutionAlgorithmParameters()
+        {
+            var evolutionAlgorithmParametersFilePath = $"{FolderPrefix}{EvolutionAlgorithmParametersFileName}";
+
+            return ReadSettings<NeatEvolutionAlgorithmParameters>(evolutionAlgorithmParametersFilePath);
+        }
+
+        public NeatEvolutionAlgorithmParameters ReadDefaultEvolutionAlgorithmParameters()
+        {
+            var defaultEvolutionAlgorithmParametersFilePath = $"{FolderPrefix}{DefaultPrefix}{EvolutionAlgorithmParametersFileName}";
+
+            return ReadSettings<NeatEvolutionAlgorithmParameters>(defaultEvolutionAlgorithmParametersFilePath);
+        }
+
+        public TrainingGameSettings ReadTrainingGameSettings()
+        {
+            var trainingGameSettingsFilePath = $"{FolderPrefix}{TrainingGameSettingsFileName}";
+
+            return ReadSettings<TrainingGameSettings>(trainingGameSettingsFilePath);
+        }
+
+        public TrainingGameSettings ReadDefaultTrainingGameSettings()
+        {
+            var defaultTrainingGameSettingsFilePath = $"{FolderPrefix}{DefaultPrefix}{TrainingGameSettingsFileName}";
+
+            return ReadSettings<TrainingGameSettings>(defaultTrainingGameSettingsFilePath);
+        }
+
+        public void SaveSettings(TrainerViewModel trainerViewModel)
+        {
+            SaveSettings(trainerViewModel.ExperimentSettings, $"{FolderPrefix}{ExperimentSettingsFileName}");
+            SaveSettings(trainerViewModel.NeatEvolutionAlgorithmParameters, $"{FolderPrefix}{EvolutionAlgorithmParametersFileName}");
+            SaveSettings(trainerViewModel.TrainingGameSettings, $"{FolderPrefix}{TrainingGameSettingsFileName}");
+        }
+
+        private static T ReadSettings<T>(string filePath)
+        {
+            var settingsJson = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<T>(settingsJson);
+        }
+
+        private static void SaveSettings<T>(T settings, string filePath)
+        {
+            var settingsJson = JsonConvert.SerializeObject(settings, Formatting.Indented);
+            File.WriteAllText(filePath, settingsJson);
+        }
+    }
+}
--- Player.AI.Neat.Trainer.Gui/TrainerWindow.xaml.cs
+++ Player.AI.Neat.Trainer.Gui/TrainerWindow.xaml.cs
@@ -4,19 +4,38 @@ namespace Player.AI.Neat.Trainer.Gui
 {
     public partial class TrainerWindow : Window
     {
-        public TrainerViewModel TrainerViewModel { get; }
+        private readonly JsonSettingsIO _jsonSettingsIo;
+
+        public TrainerViewModel TrainerViewModel { get; private set; }
 
         public TrainerWindow()
         {
             InitializeComponent();
 
-            var jsonSettingsReader = new JsonSettingsReader();
+            _jsonSettingsIo = new JsonSettingsIO();
 
             TrainerViewModel = new TrainerViewModel
             {
-                TrainingGameSettings = jsonSettingsReader.ReadTrainingGameSettings(),
-                ExperimentSettings = jsonSettingsReader.ReadExperimentSettings(),
-                NeatEvolutionAlgorithmParameters = jsonSettingsReader.ReadEvolutionAlgorithmParameters()
+                TrainingGameSettings = _jsonSettingsIo.ReadTrainingGameSettings(),
+                ExperimentSettings = _jsonSettingsIo.ReadExperimentSettings(),
+                NeatEvolutionAlgorithmParameters = _jsonSettingsIo.ReadEvolutionAlgorithmParameters()
+            };
+
+            DataContext = TrainerViewModel;
+        }
+
+        private void SaveSettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            _jsonSettingsIo.SaveSettings(TrainerViewModel);
+        }
+
+        private void RestoreDefaultButton_Click(object sender, RoutedEventArgs e)
+        {
+            TrainerViewModel = new TrainerViewModel
+            {
+                TrainingGameSettings = _jsonSettingsIo.ReadDefaultTrainingGameSettings(),
+                ExperimentSettings = _jsonSettingsIo.ReadDefaultExperimentSettings(),
+                NeatEvolutionAlgorithmParameters = _jsonSettingsIo.ReadDefaultEvolutionAlgorithmParameters()
             };
 
             DataContext = TrainerViewModel;
